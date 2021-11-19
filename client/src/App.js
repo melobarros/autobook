c7dabb768707be52be5a1fe72f96807dd54ec0a7
unknown <melo.barros@pucpr.edu.br>
2021-11-19 15:43:59 -0300

    Fixed landing to new React version

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={<Landing />}/>
+        </Routes>
+      </Fragment>
+    </Router>
   );
 
 export default App;
