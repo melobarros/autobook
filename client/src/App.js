065a92de612bd5bddb760a70f2384af4c6739185
unknown <melo.barros@pucpr.edu.br>
2021-11-30 11:08:18 -0300

    Added redux store

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,24 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => (
-    <Router>
-      <Fragment>
-        <Navbar />
-        
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 
 export default App;
