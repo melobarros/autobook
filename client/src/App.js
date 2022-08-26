b198d3306055855452c22b8e65eb87cbb23eaa76
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2022-08-26 10:34:31 -0300

    Added alert component and call

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -13,15 +14,13 @@ import store from './store';
 const App = () => (
     <Provider store={store}>
       <Router>
-        <Fragment>
           <Navbar />
-          
+          <Alert />
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="register" element={<Register />} />
             <Route path="login" element={<Login />} />
           </Routes>
-        </Fragment>
       </Router>
     </Provider>
   );
